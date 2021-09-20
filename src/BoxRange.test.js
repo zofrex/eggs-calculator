1065ab488714f6bc1e247f012b0bf8d90fff1240
James 'zofrex' Sanderson <zofrex@gmail.com>
2021-09-20 20:11:12 +0100

    Remove enzyme and replace it with testing-library/react, update everything

--- src/BoxRange.test.js
+++ src/BoxRange.test.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import {render, screen} from '@testing-library/react'
 import { BoxRange } from './App';
 
 it('renders without crashing', () => {
-  mount(<BoxRange />);
+  render(<BoxRange />);
 });
 
 it('renders 1 3% box', () => {
-  const result = mount(<p><BoxRange size="3" minimum="1" maximum="1"/></p>);
-  expect(result).toHaveText("1 3% chicken box");
+  render(<p><BoxRange size="3" minimum="1" maximum="1"/></p>);
+  screen.fin
+  expect(screen.getByText(/chicken/)).toHaveTextContent("1 3% chicken box");
 });
 
 it('renders 1 12% box', () => {
-  const result = mount(<p><BoxRange size="12" minimum="1" maximum="1"/></p>);
-  expect(result).toHaveText("1 12% chicken box");
+  render(<p><BoxRange size="12" minimum="1" maximum="1"/></p>);
+  expect(screen.getByText(/chicken/)).toHaveTextContent("1 12% chicken box");
 });
 
 it('renders 2 boxes', () => {
-  const result = mount(<p><BoxRange size="3" minimum="2" maximum="2"/></p>);
-  expect(result).toHaveText("2 3% chicken boxes");
+  render(<p><BoxRange size="3" minimum="2" maximum="2"/></p>);
+  expect(screen.getByText(/chicken/)).toHaveTextContent("2 3% chicken boxes");
 });
 
 it('renders a range of boxes', () => {
-  const result = mount(<p><BoxRange size="3" minimum="2" maximum="4"/></p>)
-  expect(result).toHaveText("2–4 3% chicken boxes");
+  render(<p><BoxRange size="3" minimum="2" maximum="4"/></p>)
+  expect(screen.getByText(/chicken/)).toHaveTextContent("2–4 3% chicken boxes");
 });
